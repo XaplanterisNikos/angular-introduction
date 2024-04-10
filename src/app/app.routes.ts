6e3cf803acd0fb0b9c3b5a0b8e7679f5a4438eb6
N. Xaplanteris <xaplanterisnikos@gmail.com>
2024-04-11 01:50:45 +0300

    template driven form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ComponentInputExampleComponent } from './components/component-input-exa
 import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
 import { SimpleDatabaseExampleComponent } from './components/simple-database-example/simple-database-example.component';
 import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
+import { TemplateDrivenFormExampleComponent } from './components/template-driven-form-example/template-driven-form-example.component';
 
 export const routes: Routes = [
     {path:'event-bind-example', component: EventBindExampleComponent},
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path:'component-input-example',component: ComponentInputExampleComponent},
     {path:'for-directive-example',component:ForDirectiveExampleComponent},
     {path:'simple-datatable-example',component:SimpleDatabaseExampleComponent},
-    {path:'component-output-example',component:ComponentOutputExampleComponent}
+    {path:'component-output-example',component:ComponentOutputExampleComponent},
+    {path:'template-driven-form-example',component:TemplateDrivenFormExampleComponent}
 ];
