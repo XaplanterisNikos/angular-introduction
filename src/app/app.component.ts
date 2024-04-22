880e06b9395f1830be810a3dd467351e14371ddc
N. Xaplanteris <xaplanterisnikos@gmail.com>
2024-04-22 21:10:42 +0300

    tokken login navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ListGroupMenuComponent,RouterLink,RouterOutlet],
+  imports: [ListGroupMenuComponent,RouterLink,RouterOutlet,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
